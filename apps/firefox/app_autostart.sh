4e3ce5802d55614e47b7fc40b1297d9f61e5f39b
sion10032 <sion10032@hotmail.com>
2026-08-10 16:53:53 +0800

    feat: migrate firefox image to icewm

--- apps/firefox/app_autostart.sh
+++ apps/firefox/app_autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Openbox 应用自启钩子：启动 Firefox
-# 使用 pgrep 防止 openbox 重启导致 Firefox 重复启动
+# IceWM 应用自启钩子：启动 Firefox
+# 使用 pgrep 防止 icewm 重启导致 Firefox 重复启动
 if ! pgrep -x firefox > /dev/null 2>&1; then
     firefox >/dev/null 2>&1 &
 fi
